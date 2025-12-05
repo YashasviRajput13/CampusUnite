052ec58aba2aa62a439af7fd482f9b84dca871f8
Yashasvi <ryashasvi77@gmail.com>
2025-12-05 20:20:07 +0530

    updated index.js

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Rendering main App component into root element
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+console.log("Index.js updated and running successfully!");
